37f909fb803dd700cc799d116402b9dde8381b48
Ryan Burke <theryanburke@gmail.com>
2012-11-09 14:05:26 -0700

    first commit of basic app

---
+++ app/controllers/Application.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void hi() {
+        render();
+    }
+
+    public static void foo() {
+        render();
+    }
+
+    public static void bar() {
+        render();
+    }
+
+}
